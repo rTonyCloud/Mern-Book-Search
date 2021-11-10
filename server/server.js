49d07fca958d07a9b2607aefc033dbc7f55f43fb
Tony Rivera <rtonycloud@gmail.com>
2021-11-10 18:56:07 -0500

    redeploy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+require('dotenv').config(); 
 console.log(process.env.MONGO_URI)
 
 const {ApolloServer} = require('apollo-server-express');
